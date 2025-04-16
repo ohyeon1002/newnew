2058c3e45138d7734719a345d6a03e687a8c1de2
ohyeon1002 <skyblueazure4@gmail.com>
2025-04-16 13:10:44 +0900

    choicev1

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Choice from "./choicesaver/Choice";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className="w-full h-screen">
+        <header
+          className="w-full h-1/9
+                    flex flex-row justify-center items-center
+                   bg-amber-50 "
+        >
+          <span className="text-2xl font-bold">title here</span>
+        </header>
+        <main
+          className="w-full h-7/9
+                    flex flex-row justify-center items-center
+                    bg-amber-100"
+        >
+          <Routes>
+            <Route path="/" element={<Choice />} />
+          </Routes>
+        </main>
+        <footer
+          className="w-full h-1/9
+                    flex flex-row justify-center items-center
+                  bg-amber-50 "
+        >
+          <span className="text-xl font-bold italic">tail here</span>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
-
-export default App
---
+++ src/choicesaver/Choice.tsx
@@ -0,0 +1,11 @@
+export default function Choice() {
+  return (
+    <div
+      className="w-1/2 h-1/2
+                flex flex-row
+                border rounded-md border-gray-300"
+    >
+      <span>hi</span>
+    </div>
+  );
+}
